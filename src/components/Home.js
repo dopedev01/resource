0b5254187b09a479d9bf9f1df2ba8b2abbaaae83
dopedev01 <rayanmuhammad798@gmail.com>
2022-09-11 00:15:50 +0530

    added a admin login

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ export default function Home() {
+   return (
+     <div>
+
+<h1>I am a Home</h1>
+
+     </div>
+   )
+ }
+ 
---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import Home from "./Home";
+import './form.css'
+
+function Login() {
+    const [inputs, setInputs] = useState({});
+    const [isLogin,setisLogin]=useState(false);
+
+
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const handle = (event) => {
+        event.preventDefault();
+        console.log(inputs);
+        if (inputs.email === "admin@gmail.com" && inputs.password === '123456') {
+            setisLogin(true)
+
+
+        } else {
+             alert("wrong")
+        }
+
+    }
+
+    return (
+        <div>
+            {!isLogin ?
+            <div className="form">
+             <h1>Admin Login</h1>
+                <form onSubmit={handle}>
+                <label>Email</label>
+                <br/>
+                    <input
+                        type="email"
+                        name="email"
+                        value={inputs.email} 
+                        onChange={handleChange}
+                    />
+                <br/>
+                <label>Password</label>
+                <br/>
+                    <input id="la"
+                        type="password"
+                        name="password"
+                        value={inputs.password}
+                        onChange={handleChange}
+                    />
+                <br/>
+                <button id="btn" onClick={handle}>Login</button>
+            </form> </div> 
+            :
+           <Home/>}
+
+
+
+        </div>
+    )
+}
+
+export default Login;
